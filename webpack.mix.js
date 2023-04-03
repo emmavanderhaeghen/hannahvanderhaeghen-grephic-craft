0e6e36e0f187e470c32d11aae90aa2294641d5cc
pgm-emmavanderhaeghen <vanderhaeghenemma@gmail.com>
2023-04-03 18:26:43 +0200

    Craft setup

---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+const mix = require("laravel-mix");
+
+mix
+  .js("./src/scripts/index.js", "app.js")
+  .css("./src/styles/styles.css", "app.css")
+  .setPublicPath("./web/dist")
+  .version();
+
+mix.browserSync({
+  proxy: "https://hannahvanderhaeghen-grephic-craft.ddev.site/",
+  https: true,
+  ui: false,
+  open: false,
+  notify: false,
+});
