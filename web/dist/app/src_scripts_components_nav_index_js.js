159b5ece168f1d3f6ccdbdf9d2b894f0e979887c
pgm-emmavanderhaeghen <vanderhaeghenemma@gmail.com>
2023-04-04 11:28:06 +0200

    web dist generated

---
+++ web/dist/app/src_scripts_components_nav_index_js.js
@@ -0,0 +1,13 @@
+(self["webpackChunkhannahvanderhaeghen"] = self["webpackChunkhannahvanderhaeghen"] || []).push([["src_scripts_components_nav_index_js"],{
+
+/***/ "./src/scripts/components/nav/index.js":
+/*!*********************************************!*\
+  !*** ./src/scripts/components/nav/index.js ***!
+  \*********************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
