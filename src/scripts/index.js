28ee85d1aef9c96d859b7b290e9d9a7e38a9b29f
pgm-emmavanderhaeghen <vanderhaeghenemma@gmail.com>
2023-04-04 12:04:48 +0200

    Made functional nav (routing)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,44 @@
 // import './components/grephic-log';
 // import './components/nav';
+import { gsap } from "gsap";
 class App {
   constructor() {
+    this.button = document.querySelector('.js-nav-button');
+    this.lines = document.querySelectorAll('.js-lines');
+    this.nav = document.querySelector('.js-nav');
+
     this.init();
   }
 
   init() {
     this.log();
+    this.addEventListeners();
   }
 
+  addEventListeners() {
+    this.button.addEventListener('click', () => {
+      this.button.classList.toggle('open');
+      this.nav.classList.toggle('hidden');
+      if (this.button.classList.contains('open')) {
+        gsap.to(this.lines[0], { duration: 0.2, rotate: '45deg', top: '8px' });
+        gsap.to(this.lines[1], { duration: 0.2, rotate: '-45deg', bottom: '2px' });
+      } else {
+        gsap.to(this.lines[0], { duration: 0.2, rotate: '0deg', top: '0' });
+        gsap.to(this.lines[1], { duration: 0.2, rotate: '0deg', top: '0' });
+      }
+    });
+  } 
+
   log() {
-    console.clear();
+    // console.clear();
     console.log('\ngrephic was here\nhttps://pgm-emmavanderhaeghen.github.io/grephic\n ');
   }
 }
 
 new App();
+
+
+
+
+
+
