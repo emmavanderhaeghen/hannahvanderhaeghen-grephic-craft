2fa990d36f17bd640481c958329cc4c6a3769084
pgm-emmavanderhaeghen <vanderhaeghenemma@gmail.com>
2023-04-04 11:27:25 +0200

    setup scripts

---
+++ src/scripts/components/grephic-log/index.js
@@ -0,0 +1,14 @@
+export default class GrephicLog {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.log();
+  }
+
+  log() {
+    console.clear();
+    console.log('\nspiced up by grephic.be\nhttps://pgm-emmavanderhaeghen.github.io/grephic\n ');
+  }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,18 @@
-// simple alert 
-// alert("Hello World");
+// import './components/grephic-log';
+// import './components/nav';
+class App {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.log();
+  }
+
+  log() {
+    console.clear();
+    console.log('\ngrephic was here\nhttps://pgm-emmavanderhaeghen.github.io/grephic\n ');
+  }
+}
+
+new App();
