27ed7fdd1d44211a01cdbf438a99d4c8e982f9a6
pgm-emmavanderhaeghen <vanderhaeghenemma@gmail.com>
2023-04-13 13:45:01 +0200

    added custom modules

--- config/app.php
+++ config/app.php
@@ -24,4 +24,13 @@ use craft\helpers\App;
 
 return [
     'id' => App::env('CRAFT_APP_ID') ?: 'CraftCMS',
+
+    'modules' => [
+        'business-logic-module' => \modules\businesslogicmodule\BusinessLogicModule::class,
+        'shop-adjusters-module' => \modules\shopadjustersmodule\ShopAdjustersModule::class,
+    ],
+    'bootstrap' => [
+        'business-logic-module',
+        'shop-adjusters-module',
+    ],
 ];
