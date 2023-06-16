700757a89b669cfaad008ed0867b224114c2f5ca
pgm-emmavanderhaeghen <vanderhaeghenemma@gmail.com>
2023-06-16 16:12:48 +0200

    added translations

---
+++ translations/fr/site.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    "E-mail address" => "Adresse e-mail",
+    "Password" => "Mot de passe",
+    "Addresses" => "Adresses",
+    "Change E-mail" => "Modifier l'adresse e-mail",
+    "Change Password" => "Modifier le mot de passe",
+    "Log out" => "Déconnexion",
+    "Login" => "Connexion",
+    "Register" => "Registre",
+
+    "Your shopping bag is still empty." => "Votre sac de courses est encore vide.",
+    "Pay" => "Payer",
+    "Information" => "Informations",
+    "Payment" => "Paiement",
+    "Add to my bag" => "Ajouter à mon sac",
+    "Come back when we have more..." => "Revenez quand nous en aurons plus...",
+    "Your whislist is still empty." => "Votre liste de diffusion est encore vide.",
+    "remove from wishlist" => "supprimer de la liste de diffusion",
+    "add to wishlist" => "ajouter à la liste de diffusion",
+];
+
+
+
---
+++ translations/nl/site.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    "E-mail address" => "E-mail adres",
+    "Password" => "Passwoord",
+    "Addresses" => "Adressen",
+    "Change E-mail" => "Verander e-mail",
+    "Change Password" => "Verander passwoord",
+    "Log out" => "Log uit",
+    "Login" => "Log in",
+    "Register" => "Registreer",
+
+    "Your shopping bag is still empty." => "Je winkelmandje is nog leeg.",
+    "Pay" => "Betaal",
+    "Information" => "Informatie",
+    "Payment" => "Betalen",
+    "Add to my bag" => "Toevoegen aan mijn mandje",
+    "Come back when we have more..." => "Kom terug als we meer hebben...",
+    "Your whislist is still empty." => "Je verlanglijstje is nog leeg.",
+    "remove from wishlist" => "verwijderen van verlanglijstje",
+    "add to wishlist" => "toevoegen aan verlanglijstje",
+];
